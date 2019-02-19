f32b93d088702b36ea98e1cd676a99680311a193
Fatima Sahmoud <fatimasahmouod@hotmail.com>
2019-02-19 12:24:21 +0200

    final before some modifications

--- js/main.js
+++ js/main.js
@@ -1,6 +1,56 @@
-// $(function () {
-//     'use strict';
 
-//     var winH =$(window).height();
-//     $('.about-slider').height(winH);
-// });
+$("document").ready(function($){
+
+// navbar change background
+    var nav = $('#mainnav');
+    $(window).scroll(function () {
+		
+        if ($(this).scrollTop() > 0) {
+            nav.addClass("sticky");
+        } else {
+            nav.removeClass("sticky");
+        }
+        });
+   
+    // scroll top 
+	var scrollBtn = $(".scroll-up");
+	$(window).scroll(function () {
+			if ($(this).scrollTop()>=100)
+			{
+				scrollBtn.show();
+			}
+			else{
+                scrollBtn.hide();
+            }             
+		});
+		scrollBtn.click(function () {
+			$("html, body").animate({scrollTop: 0}, 600);
+			return false;
+			});
+
+
+// 			var $p = $('.member-info .more__info');
+// var divh = $('.member-info').height();
+// while ($p.outerHeight() > divh) {
+//     $p.text(function (index, text) {
+//         return text.replace(/\W*\s(\S)*$/, '...');
+//     });
+// }
+
+ }); //end ready function
+
+//  alert(navigator.userAgent);
+// if ((this.navigator.userAgent) == "Mozilla") {
+
+
+// var $p = $('.member-info .more__info');
+// var divh = $('.member-info').height();
+// while ($p.outerHeight() > divh) {
+//     $p.text(function (index, text) {
+//         return text.replace(/\W*\s(\S)*$/, '...');
+//     });
+// }
+// }
+
+
+
